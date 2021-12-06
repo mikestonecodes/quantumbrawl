57b164baf9d939ee16c20c5da77d07de05757e0f
mikestonecodes <michaeljacobstone@gmail.com>
2021-12-06 19:41:22 +0100

    show hacks

--- components/player.js
+++ components/player.js
@@ -11,12 +11,13 @@ AFRAME.registerComponent('player', {
         console.log("my gun ID", this.gunid);
         this.el.gunid = this.gunid;
         this.lpos = this.el.object3D.position;
-
+        window.gunid = this.gunid;
         document.getElementById("teleportButton").addEventListener("click", ()=>{
-          let ng = Math.random() > .5?-1:1;
 
-          let rnd = (5+Math.random()*10)*ng
-          this.el.object3D.position.set(rnd,0.5,rnd);
+          let ng = Math.random() > .5?-1:1;
+          let ng2 = Math.random() > .5?-1:1;
+          this.el.object3D.position.set(Math.random()*18*ng,0.5,Math.random()*18*ng2);
+          
         });
 
       },
